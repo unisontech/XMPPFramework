849b1e3666e716ba41f762c250c5dbb50d84c3a2
Maxim V. Potapov <mpotapov@unison.com>
2015-07-13 16:12:55 +0300

    project with ios framework target

---
+++ Xcode/XMPPFramework/XMPPFramework/XMPPFramework.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for XMPPFramework.
+FOUNDATION_EXPORT double XMPPFrameworkVersionNumber;
+
+//! Project version string for XMPPFramework.
+FOUNDATION_EXPORT const unsigned char XMPPFrameworkVersionString[];
+
+#import <XMPPFramework/XMPP.h>
